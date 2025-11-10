c5f7865fad820523b880ad8dfe71d30d92c789c6
Leandro Battochio <lbattochio@hotmail.com>
2025-11-09 21:46:49 -0300

    rate limit test

--- tests/SoftCep.Integration.Tests/Controllers/CepControllerRateLimitSoftTests.cs
+++ tests/SoftCep.Integration.Tests/Controllers/CepControllerRateLimitSoftTests.cs
@@ -12,28 +12,20 @@ public class CepControllerRateLimitSoftTests(RateLimitWebApplicationFactory fact
         const string ip = "9.9.9.9";
 
         var initialRequests = new List<HttpRequestMessage>();
-        for (var i = 0; i < 10; i++)
+        for (var i = 0; i < 21; i++)
         {
             var rCep = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/17209660");
             rCep.Headers.Add("X-Test-IP", ip);
             initialRequests.Add(rCep);
-
-            var rAddr = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/SP/Sao Paulo/Praca");
-            rAddr.Headers.Add("X-Test-IP", ip);
-            initialRequests.Add(rAddr);
         }
 
         var responses = await Task.WhenAll(initialRequests.Select(r => Client.SendAsync(r)));
-        responses.Length.ShouldBe(20);
-        responses.ShouldAllBe(r => r.StatusCode == HttpStatusCode.OK);
+        responses.Length.ShouldBe(21);
+        responses.Count(c => c.StatusCode == HttpStatusCode.OK).ShouldBe(20);
+        responses.Count(c => c.StatusCode == HttpStatusCode.TooManyRequests).ShouldBe(1);
         
-        var blocked = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/17209660");
-        blocked.Headers.Add("X-Test-IP", ip);
-        var blockedResp = await Client.SendAsync(blocked);
-        blockedResp.StatusCode.ShouldBe(HttpStatusCode.TooManyRequests);
-
         await Task.Delay(1100);
-        var afterWindow = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/SP/Sao Paulo/Praca");
+        var afterWindow = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/17209660");
         afterWindow.Headers.Add("X-Test-IP", ip);
         var afterResp = await Client.SendAsync(afterWindow);
         afterResp.StatusCode.ShouldBe(HttpStatusCode.OK);
