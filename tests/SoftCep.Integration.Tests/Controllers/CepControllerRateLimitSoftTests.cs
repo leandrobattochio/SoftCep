f3aed726f4ad673b38144ac0533ecb1678b35cd7
Leandro Battochio <lbattochio@hotmail.com>
2025-11-09 21:34:50 -0300

    fix rate limit test

--- tests/SoftCep.Integration.Tests/Controllers/CepControllerRateLimitSoftTests.cs
+++ tests/SoftCep.Integration.Tests/Controllers/CepControllerRateLimitSoftTests.cs
@@ -11,28 +11,28 @@ public class CepControllerRateLimitSoftTests(RateLimitWebApplicationFactory fact
     {
         const string ip = "9.9.9.9";
 
-        var successResponses = new List<HttpStatusCode>();
+        var initialRequests = new List<HttpRequestMessage>();
         for (var i = 0; i < 10; i++)
         {
             var rCep = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/17209660");
             rCep.Headers.Add("X-Test-IP", ip);
+            initialRequests.Add(rCep);
+
             var rAddr = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/SP/Sao Paulo/Praca");
             rAddr.Headers.Add("X-Test-IP", ip);
-
-            var responses = await Task.WhenAll(Client.SendAsync(rCep), Client.SendAsync(rAddr));
-            successResponses.Add(responses[0].StatusCode);
-            successResponses.Add(responses[1].StatusCode);
+            initialRequests.Add(rAddr);
         }
 
-        successResponses.Count.ShouldBe(20);
-        successResponses.ShouldAllBe(s => s == HttpStatusCode.OK);
+        var responses = await Task.WhenAll(initialRequests.Select(r => Client.SendAsync(r)));
+        responses.Length.ShouldBe(20);
+        responses.ShouldAllBe(r => r.StatusCode == HttpStatusCode.OK);
 
         var blocked = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/17209660");
         blocked.Headers.Add("X-Test-IP", ip);
         var blockedResp = await Client.SendAsync(blocked);
         blockedResp.StatusCode.ShouldBe(HttpStatusCode.TooManyRequests);
 
-        await Task.Delay(1050);
+        await Task.Delay(1100);
         var afterWindow = new HttpRequestMessage(HttpMethod.Get, "/api/v1/cep/SP/Sao Paulo/Praca");
         afterWindow.Headers.Add("X-Test-IP", ip);
         var afterResp = await Client.SendAsync(afterWindow);
