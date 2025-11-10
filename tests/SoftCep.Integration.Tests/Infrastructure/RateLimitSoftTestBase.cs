8d44c842079b5cfa7cb07d817d650ecee02d6ca0
Leandro Battochio <lbattochio@hotmail.com>
2025-11-10 15:09:38 -0300

    rate limits tests are unnecessary

--- tests/SoftCep.Integration.Tests/Infrastructure/RateLimitSoftTestBase.cs
+++
@@ -1,6 +0,0 @@
-namespace SoftCep.Integration.Tests.Infrastructure;
-
-public abstract class RateLimitSoftTestBase(RateLimitWebApplicationFactory factory)
-    : BaseSoftTestBase<RateLimitWebApplicationFactory>(factory)
-{
-}
